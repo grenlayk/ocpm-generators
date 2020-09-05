f9728c8421c5be19dd7d450b76af5d2f59881ee7
Kozlova Olga <grenlaykk@gmail.com>
2020-09-06 01:32:47 +0300

    Copy file

--- elementary-1.js
+++ elementary-1.js
@@ -1,19 +1,25 @@
-const { PDFDocument, StandardFonts, rgb } = PDFLib;
+// Library from https://github.com/Hopding/pdf-lib
+const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
 
 async function createPdf() {
 
   // Create a new PDFDocument
   const pdfDoc = await PDFDocument.create()
 
-  const url = 'graph.pdf';
-  
-  const firstDonorPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+  const url = 'pdf/graph.pdf';
+  const fs = require('fs');
+  const firstDonorPdfBytes = fs.readFileSync(url);
   const firstDonorPdfDoc = await PDFDocument.load(firstDonorPdfBytes);
 
   const [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [0]);
   pdfDoc.addPage(firstDonorPage);
   const pdfBytes = await pdfDoc.save();
 
+  
+  fs.writeFile('test.pdf', pdfBytes, ()=>{});
+
   // Trigger the browser to download the PDF document
-  download(pdfBytes, "test.pdf", "application/pdf");
+  //download(pdfBytes, "test.pdf", "application/pdf");
 }
+
+createPdf();
