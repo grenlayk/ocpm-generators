179849e7cf9d1d1e3c593ea58c5c8f615d5e2ed2
Kozlova Olga <grenlaykk@gmail.com>
2020-10-26 02:18:52 +0300

    Create edges list

---
+++ senior-1.js
@@ -0,0 +1,80 @@
+// Library from https://github.com/Hopding/pdf-lib
+const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
+
+// function from https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// random integer number in range [min; max]
+function getRandomInt(min, max) {
+    return min + Math.floor(Math.random() * Math.floor(max - min));
+}
+
+const xLabels = ["А", "Б", "В", "Г", "Д", "Е", "Ж"];
+const yLabels = ["1", "2", "3", "4", "5"];
+
+class Vertex {
+    constructor(id) {
+      this.i = parseInt(id / xLabels.length);
+      this.j = id % xLabels.length;
+    }
+
+    label() {
+        return xLabels[this.j] + yLabels[this.i];
+    }
+}
+
+class Edge {
+    constructor(v, u, weight) {
+      this.v = v;
+      this.u = u;
+      this.weight = weight;
+    }
+
+    logMessage() {
+        return this.v.label() + " <-> " + this.u.label() + " : " + this.weight;
+    }
+}
+
+function isNeighbor(v, u) {
+    return Math.abs(v.i - u.i) + Math.abs(v.j - u.j) == 1;
+}
+
+
+async function createEdges() {
+    let edges = [];
+    console.log("I try");
+    const numberOfCrosses = xLabels.length * yLabels.length;
+    for (let vId = 0; vId < numberOfCrosses; vId++) {
+        const v = new Vertex(vId);
+        for (let uId = 0; uId < numberOfCrosses; uId++) {
+            const u = new Vertex(uId);
+            if (isNeighbor(v, u)) {
+                edges.push(new Edge(v, u, getRandomInt(10, 50)));
+                console.log(edges[edges.length - 1].logMessage());
+            }
+        }
+    }
+}
+
+async function createPdf() {
+
+  // Create a new PDFDocument
+  const pdfDoc = await PDFDocument.create()
+
+  const url = 'pdf/graph.pdf';
+  const fs = require('fs');
+  const firstDonorPdfBytes = fs.readFileSync(url);
+  const firstDonorPdfDoc = await PDFDocument.load(firstDonorPdfBytes);
+
+  const [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [0]);
+  pdfDoc.addPage(firstDonorPage);
+  const pdfBytes = await pdfDoc.save();
+
+  fs.writeFile('test.pdf', pdfBytes, ()=>{});
+
+  console.log("File created!")
+
+  // // Trigger the browser to download the PDF document
+  // download(pdfBytes, "test.pdf", "application/pdf");
+}
+
+//createPdf();
+createEdges();
