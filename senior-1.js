13791e3d4c2e814174b472ad7b098546d71db813
Kozlova Olga <grenlaykk@gmail.com>
2020-10-28 16:13:55 +0300

    It's workinggit statusgit status

--- senior-1.js
+++ senior-1.js
@@ -1,5 +1,5 @@
 // Library from https://github.com/Hopding/pdf-lib
-const { PDFDocument, StandardFonts, rgb } = require('pdf-lib');
+const { PDFDocument, StandardFonts, rgb, degrees} = require('pdf-lib');
 
 // function from https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // random integer number in range [min; max]
@@ -10,6 +10,13 @@ function getRandomInt(min, max) {
 const xLabels = ["А", "Б", "В", "Г", "Д", "Е", "Ж"];
 const yLabels = ["1", "2", "3", "4", "5"];
 const numberOfCrosses = xLabels.length * yLabels.length;
+
+const cellSize = 568;
+
+const hLeftTopX = 3020; // for horizontal lines
+const hLeftTopY = 2815; // for horizontal lines
+const vLeftTopX = 2610; // for vertical lines
+const vLeftTopY = 2430; // for vertical lines
 
 class Vertex {
     constructor(id) {
@@ -43,7 +50,7 @@ function createEdges() {
     let edges = [];
     for (let vId = 0; vId < numberOfCrosses; vId++) {
         const v = new Vertex(vId);
-        for (let uId = 0; uId < numberOfCrosses; uId++) {
+        for (let uId = vId + 1; uId < numberOfCrosses; uId++) {
             const u = new Vertex(uId);
             if (isNeighbor(v, u)) {
                 edges.push(new Edge(v, u, getRandomInt(10, 50)));
@@ -81,27 +88,67 @@ function printTable(edgesTable) {
     console.table(edgesTable);
 }
 
+function draw(page, font, label, x, y) {
+    page.drawText(label, {
+        x: x,
+        y: y,
+        size: 150,
+        font: font,
+        color: rgb(0.95, 0.1, 0.1),
+    })
+}
+
 async function createPdf() {
-  // Create a new PDFDocument
-  const pdfDoc = await PDFDocument.create()
-
+  // Field pdf to modify
   const url = 'pdf/graph.pdf';
+
+  // fs for file reading
   const fs = require('fs');
-  const firstDonorPdfBytes = fs.readFileSync(url);
-  const firstDonorPdfDoc = await PDFDocument.load(firstDonorPdfBytes);
 
-  const [firstDonorPage] = await pdfDoc.copyPages(firstDonorPdfDoc, [0]);
-  pdfDoc.addPage(firstDonorPage);
-  const pdfBytes = await pdfDoc.save();
+  // This should be a Uint8Array or ArrayBuffer
+  // This data can be obtained in a number of different ways
+  // If your running in a Node environment, you could use fs.readFile()
+  // In the browser, you could make a fetch() call and use res.arrayBuffer()
+  const pdfBytes = fs.readFileSync(url);
+  const pdfDoc = await PDFDocument.load(pdfBytes);
 
-  fs.writeFile('test.pdf', pdfBytes, ()=>{});
+  // Get the first page of the document
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
 
-  console.log("File created!")
+  // Modify pdf here
+
+  // Embed the Helvetica font
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  const edges = createEdges();
+  for (edge of edges) {
+      if (edge.v.j == edge.u.j) { // vertical edge
+        const x = vLeftTopX + cellSize * edge.v.j;
+        const y = vLeftTopY - cellSize * Math.min(edge.v.i, edge.u.i);
+        const label = edge.weight;
+        draw(firstPage, helveticaFont, label.toString(), x, y);
+      } else {
+        const x = hLeftTopX + cellSize * Math.min(edge.v.j, edge.u.j)
+        const y = hLeftTopY - cellSize * edge.v.i;
+        const label = edge.weight;
+        draw(firstPage, helveticaFont, label.toString(), x, y);
+      }
+      //console.log(edge);
+  }
+
+  // Save pdf
+  const pdfResultBytes = await pdfDoc.save();
+
+  // Create result pdf
+  fs.writeFile('senior-1.pdf', pdfResultBytes, ()=>{});
+
+  //console.log("File created!") 
 
   // // Trigger the browser to download the PDF document
   // download(pdfBytes, "test.pdf", "application/pdf");
 }
 
-//createPdf();
-const edgesTable = createEdgesTable();
-printTable(edgesTable);
+createPdf();
+// const edgesTable = createEdgesTable();
+// printTable(edgesTable);
