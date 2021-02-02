4b9547bc2a5b5c533cdbeb083610f079b1c0b4e3
Kozlova Olga <grenlaykk@gmail.com>
2021-02-02 23:15:55 +0300

    Restruct junior-2

--- junior/junior-2.js
+++ junior/junior-2.js
@@ -1,29 +1,31 @@
-let col_ids = [0, 1, 0, 1, 0, 1, 0, 1, 0];
-let idx = null;
-const total = 28;
-let chosen = 9;
-let chosen_colors = [0, 1, 2, 3];
+let colorIds = [0, 1, 0, 1, 0, 1, 0, 1, 0];
+let ids = null;
+const N = 28;
+let K = 9;
+let C_COLORS = [0, 1, 2, 3, 5];
 
-const leftCorner = 937 - 24;
-const yHight = 1605 - 24;
-const dt = 175.7;
-const cubeWidth = 142.5;//95
+const X_CORNER = 937 - 24;
+const Y_CORNER = 1605 - 24;
+const DLT = 175.7;
+const C_WIDTH = 142.5;//95
 
+// print chosen colors
 function printColors() {
     let label = document.getElementById('label');
-    label.innerText = `Выбранные цвета: ${names[chosen_colors[0]]}, ${names[chosen_colors[1]]}`;
+    label.innerText = `Выбранные цвета: ${names[C_COLORS[0]]}, ${names[C_COLORS[1]]}`;
 }
 
+// draw cubes on the field
 function drawCubes(page, font) {
     const textSize = 120;
-    for (let i = 0; i < chosen; ++i) {
-        let x = leftCorner + dt * idx[i];
-        let y = yHight;
-        let id = chosen_colors[col_ids[i]];
+    for (let i = 0; i < K; ++i) {
+        let x = X_CORNER + DLT * ids[i];
+        let y = Y_CORNER;
+        let id = C_COLORS[colorIds[i]];
         const textWidth = font.widthOfTextAtSize(letters[id], textSize);
-        const del = (cubeWidth - textWidth) / 2 - 6;
-        drawBox(page, x, y, colors[id], cubeWidth);
-        drawText(page, letters[id], x + del, y, textSize, font, blackColor);
+        const delta = (C_WIDTH - textWidth) / 2 - 6;
+        drawBox(page, x, y, colors[id], C_WIDTH);
+        drawText(page, letters[id], x + delta, y, textSize, font, blackColor);
     }
 }
 
@@ -37,48 +39,49 @@ async function createFieldPdf(filename) {
 
     drawCubes(pages[0], font);
 
-    pdfDoc.setTitle('Field');
+    pdfDoc.setTitle('Junior-2');
     pdfDoc.setAuthor('mosrobotics');
 
     const pdfResultBytes = await pdfDoc.save();
     renderInIframe(pdfResultBytes, filename);
 }
 
+// add 1 / 2 / 3 / 6 cubes of color num to colorIds
 function add(num, mx=4) {
     let cnt = getRandomInt(1, mx + 1);
     if (cnt == 4) {
         cnt = 6;
     }
     for (let i = 0; i < cnt; ++i) {
-        col_ids.push(num);
+        colorIds.push(num);
     }
-    col_ids.shuffle();
+    colorIds.shuffle();
+}
+
+// choose colors and positions
+function chooseIds() {
+    add(2);
+    add(3);
+    let border = Math.min(N - colorIds.length, 6);
+    if (border == 4 || border == 5) {
+        border = 3;
+    }
+    add(4, border);
+    K = colorIds.length;
+    ids = getCnk(N, K);
+    C_COLORS.shuffle();
 }
 
 async function createField() {
-    if (idx == null) {
-        col_ids = [0, 0, 0, 0, 0, 1, 1, 1, 1];
-        chosen_colors = [0, 1, 2, 3, 5];
-        add(2);
-        add(3);
-        let border = Math.min(total - col_ids.length, 6);
-        if (border == 4 || border == 5) {
-            border = 3;
-        }
-        add(4, border);
-        chosen = col_ids.length;
-        idx = getCnk(total, chosen);
-        console.log(idx);
-        console.log(col_ids);
-
-
-        chosen_colors.shuffle();
+    if (ids == null) {
+        colorIds = [0, 0, 0, 0, 0, 1, 1, 1, 1];
+        chooseIds();
         printColors();
     }
     await createFieldPdf('field');
 }
 
 function refreshPage() {
-    idx = null;
+    ids = null;
     createField();
 }
